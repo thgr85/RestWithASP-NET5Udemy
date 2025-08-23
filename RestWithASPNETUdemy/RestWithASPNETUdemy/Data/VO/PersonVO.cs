b56cab835258b50615c5eeaa88515f98b216412e
Thiago Oliveira <thgr85@outlook.com>
2025-08-23 14:59:42 -0300

    chore: serialization

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -1,11 +1,18 @@
-﻿namespace RestWithASPNETUdemy.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithASPNETUdemy.Data.VO
 {
     public class PersonVO
     {
+        [JsonPropertyName("id")]
         public long Id { get; set; }
+        [JsonPropertyName("first_name")]
         public string? FirstName { get; set; }
+        [JsonPropertyName("last_name")]
         public string? LastName { get; set; }
+        [JsonIgnore]
         public string? Address { get; set; }
+        [JsonPropertyName("gender")]
         public string? Gender { get; set; }
     }
 }
