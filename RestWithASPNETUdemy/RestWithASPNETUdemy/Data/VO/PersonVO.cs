31c358b3e5687f86b3b6e615c689cd485051bf9e
Thiago Oliveira <thgr85@outlook.com>
2025-08-23 15:23:28 -0300

    chore: content negotiation

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -1,18 +1,11 @@
-﻿using System.Text.Json.Serialization;
-
-namespace RestWithASPNETUdemy.Data.VO
+﻿namespace RestWithASPNETUdemy.Data.VO
 {
     public class PersonVO
     {
-        [JsonPropertyName("id")]
         public long Id { get; set; }
-        [JsonPropertyName("first_name")]
         public string? FirstName { get; set; }
-        [JsonPropertyName("last_name")]
         public string? LastName { get; set; }
-        [JsonIgnore]
         public string? Address { get; set; }
-        [JsonPropertyName("gender")]
         public string? Gender { get; set; }
     }
 }
